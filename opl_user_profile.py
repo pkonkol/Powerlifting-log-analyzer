84fbd95c4536fd0c76936e5930e3ead76c451906
Lajzer <Piotrkonkol01@gmail.com>
2019-07-03 19:40:57 +0200

    run and profile skeletons

---
+++ opl_user_profile.py
@@ -0,0 +1,14 @@
+
+
+class Profile:
+    self.available_workout_days = ''
+    self.available_equipment = ''
+    self.blacklisted_exercises = ''
+    self.favorite_exercises = ''
+    self.past_injuries = ''
+    self.birthday = ''
+    self.gender = ''
+
+    def __init__(self):
+        pass
+
