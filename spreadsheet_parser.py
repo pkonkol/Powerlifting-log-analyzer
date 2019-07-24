45e4e041ff102f03e2f11f574774fe37ff980574
Lajzer <Piotrkonkol01@gmail.com>
2019-07-24 22:15:56 +0200

    spreadsheet parsing functions as class methods, settings planned from txt in Exercise update

--- spreadsheet_parser.py
+++ spreadsheet_parser.py
@@ -6,96 +6,98 @@ XSL_FILE = 'data/program.xlsx'
 UNITS = 'KG'
 
 RPE_SCHEME = '((?:[1-9],[5])|(?:[1-9]|10))'
-WEIGHT_SCHEME = '([0-9]+)'
+WEIGHT_SCHEME = '([0-9]+)(kg|lbs)?'
+PERCENTAGE_SCHEME = '([1-9][0-9]?|100)'
 REPS_SCHEME = '([0-9]+)'
+SETS_SCHEME = '([0-9]+)'
 
-class Training_spreadsheet_parser():
-    pass
+class TrainingSpreadsheetParser():
 
+    def parse_worksheet():
+        pass
 
-def parse_exercise(ws, col, row):
-    e = Exercise()
-    name = str(ws[col+row].value)
-    log = str(ws[chr(ord(col)+1) + row].value)
-    comment = str(ws[chr(ord(col)+1) + row].comment)
+    def parse_exercise(ws, col, row):
+        e = Exercise()
+        name = str(ws[col+row].value)
+        log = str(ws[chr(ord(col)+1) + row].value)
+        comment = str(ws[chr(ord(col)+1) + row].comment)
 
-    print(name + " :: " + log)
-    row = str(int(row)+1)
-    return w,col, row
+        print(name + " :: " + log)
+        row = str(int(row)+1)
+        return w,col, row
 
-def parse_workout(ws, start_col, start_row, lrow, day):
-    w = Workout(0, '', [], 0, )
-    col, row = start_col, start_row
-    date = str(ws[chr(ord(col)+1) + row].value)
-    print("Day " + day + " at date " + date)
+    def parse_workout(ws, start_col, start_row, lrow, day):
+        w = Workout()
+        col, row = start_col, start_row
+        date = str(ws[chr(ord(col)+1) + row].value)
+        print("Day " + day + " at date " + date)
 
-    while int(row) < int(lrow):
-        col, row = parse_exercise(ws, col, row)
+        while int(row) < int(lrow):
+            col, row = parse_exercise(ws, col, row)
 
-    col = chr(ord(col)+2)
-    row = start_row
-    return w, col, row
+        col = chr(ord(col)+2)
+        row = start_row
+        return w, col, row
 
-def parse_microcycle(ws, start_col, start_row):
-    micro = Microcycle(0,0, [], "","")
-    frow, lrow = 0, 0
-    week = 0
-    nweek = 0
-    col, row = start_col, start_row
-    m = None
-    while not m:
-        m = re.match("^W([0-9]+)$", str(ws[col+row].value))
-        if m:
-            frow = row
-            week = m.group(1)
-            week_comment = ws[col+row].comment
-            print('week = ' + str(week))
+    def parse_microcycle(ws, start_col, start_row):
+        micro = Microcycle(0,0, [], "","")
+        frow, lrow = 0, 0
+        week = 0
+        nweek = 0
+        col, row = start_col, start_row
+        m = None
+        while not m:
+            m = re.match("^W([0-9]+)$", str(ws[col+row].value))
+            if m:
+                frow = row
+                week = m.group(1)
+                week_comment = ws[col+row].comment
+                print('week = ' + str(week))
 
-            if int(week) == 0:
-                return micro, start_col, lrow, week, nweek
+                if int(week) == 0:
+                    return micro, start_col, lrow, week, nweek
 
-            while True:
+                while True:
+                    row = str(int(row) + 1)
+                    m = re.match("^W([0-9]+)$", str(ws[col+row].value))
+                    if m:
+                        nweek = m.group(1)
+                        lrow = str(int(row) - 1)
+                        break
+                break
+            else:
                 row = str(int(row) + 1)
-                m = re.match("^W([0-9]+)$", str(ws[col+row].value))
-                if m:
-                    nweek = m.group(1)
-                    lrow = str(int(row) - 1)
-                    break
-            break
-        else:
-            row = str(int(row) + 1)
 
-    col = chr(ord(col)+1)
-    print(col)
+        col = chr(ord(col)+1)
+        print(col)
 
-    row=start_row
-    m = re.match("^D([0-9]+)$", str(ws[col+row].value))
-    while m:
-        day = m.group(1)
-        day_comment = ws[col+row].comment
-        col, row = parse_workout(ws, col, row, lrow, day)
+        row=start_row
         m = re.match("^D([0-9]+)$", str(ws[col+row].value))
-        print(m)
+        while m:
+            day = m.group(1)
+            day_comment = ws[col+row].comment
+            col, row = parse_workout(ws, col, row, lrow, day)
+            m = re.match("^D([0-9]+)$", str(ws[col+row].value))
+            print(m)
 
-    print('micro ret: ' + lrow + ' ' + week)
-    return micro, start_col, str(int(lrow)+1), week, nweek
+        print('micro ret: ' + lrow + ' ' + week)
+        return micro, start_col, str(int(lrow)+1), week, nweek
 
-def parse_mesocycle(ws, start_col, start_row):
-    cur_col, cur_row = start_col, start_row
-    meso = Mesocycle([], 0, 0, "")
+    def parse_mesocycle(ws, start_col, start_row):
+        cur_col, cur_row = start_col, start_row
+        meso = Mesocycle([], 0, 0, "")
 
-    while True:
-        microcycle, cur_col, cur_row, week, nweek = parse_microcycle(ws, cur_col, cur_row)
-        print("|{},-{},; week-{},nweek-{}".format(cur_col, cur_row, week, nweek))
+        while True:
+            microcycle, cur_col, cur_row, week, nweek = parse_microcycle(ws, cur_col, cur_row)
+            print("|{},-{},; week-{},nweek-{}".format(cur_col, cur_row, week, nweek))
 
-        meso.microcycles.append(microcycle)
+            meso.microcycles.append(microcycle)
 
-        print(nweek)
-        if nweek == 0:
-            break
+            print(nweek)
+            if nweek == 0:
+                break
 
-    return ":)"
-
+        return ":)"
 
 
 class Exercise:
@@ -200,17 +202,109 @@ class Exercise:
         return sets_done
 
     def sets_planned_from_string(self, sets_planned_str):
+        set_scheme = {'reps': '', 'weight':'', 'RPE': '', 'set_no': ''}
         sets_num = 0
-        schemes = {'^x(\d+)@(([1-9],[5])|([1-9]|10))$': 'reps at RPE',
-                   '^^([0-9]+)x([0-9]+)@([1-9][0-9]?|100)%$$': 'sets of reps at percentage',
-                   '^([1-9][0-9]?|100)%@(([1-9],[5])|([1-9]|10))$': 'percentage at RPE',
-                   '^x([0-9]+)(@(([1-9],[5])|([1-9]|10)))(@(([1-9],[5])|([1-9]|10)))+$': 'reps at RPE multiple', #needs furhter processing
-                   '^([0-9]+)x([0-9]+)@(([1-9],[5])|([1-9]|10))$': 'sets of reps starting at RPE',
-                   '^([0-9]+)x@(([1-9],[5])|([1-9]|10))$': 'number of sets at RPE',
-                   '^([1-9][0-9]?|100)%x([0-9]+)$': 'reps at %1RM',
-                   '^([0-9]+)x([0-9]+)@([1-9][0-9]?|100)%$': 'sets of reps at percentage',
-                   '^ $': 'Sets x kilograms at RPE',
+        schemes = {'^x{reps}@{rpe}$'.format(reps=REPS_SCHEME, rpe=RPE_SCHEME): 1, #'reps at RPE',
+                   '^{sets}x{reps}@{percentage}%$'.format(sets=SETS_SCHEME, reps=REPS_SCHEME, rpe=RPE_SCHEME): 2, # 'sets of reps at percentage',
+                   '^{percentage}%@{rpe}$': 3, # 'percentage at RPE',
+                   '^x{reps}(?:@{rpe}){{2,}}$': 4, #'reps at RPE multiple', #needs furhter processing
+                   '^{sets}x{reps}@{rpe}$':5,# 'sets of reps starting at RPE',
+                   '^{sets}x@{rpe}$': 6, #'number of sets at RPE',
+                   '^{percentage}%x{reps}$': 7, #'reps at %1RM',
+                   '^{sets}x{reps}@{percentage}%$': 8, #'sets of reps at percentage',
+                   '^{sets}x{weight}@{rpe}$': 9, #'Sets x weight at RPE',
                    }
+
+        sets_str = re.split(' |;', sets_str)
+        sets_planned = []
+        set_no = 1
+        for set_str in sets_str:
+            c_set = {'reps': '', 'weight':'', 'RPE': '', 'set_no': ''}
+            while True:
+                try:
+                    match = [(re.match(key, set_str), value) for key, value in zip(schemes.keys(), schemes.values()) if re.match(key, set_str)][0]
+                except IndexError:
+                    print('Failed to match set with any of schemes')
+                    print('Please enter correct string')
+                    print(set_str, end='')
+                    set_str = input()
+                    continue
+                break
+
+        print('{}, {} - no{}, match={}, groups={}'.format(sets_str, set_str, set_no, match, match[0].groups()))
+        if match[1] == 1:
+            c_set['reps'] = int(match[0].group(1).replace(',', '.'))
+            c_set['RPE'] = float(match[0].group(2).replace(',', '.'))
+            c_set['set_no'] = set_no
+            set_no += 1
+            sets_planned.append(c_set)
+        elif match[1] == 2:
+            multiset_sets = int(match[0].group(1).replace(',', '.'))
+            multiset_reps = int(match[0].group(2).replace(',', '.'))
+            multiset_weight = float(match[0].group(3).replace(',','.'))
+            for _ in range(0,multiset_sets):
+                c_set = {'reps': '', 'weight':'', 'RPE': '', 'set_no': ''}
+                c_set['weight'] = multiset_weight
+                c_set['reps'] = multiset_reps
+                c_set['set_no'] = set_no
+                set_no += 1
+                sets_done.append(c_set)
+        elif match[1] == 3:
+            c_set['weight'] = float(match[0].group(1).replace(',','.'))
+            c_set['RPE'] = float(match[0].group(2).replace(',', '.'))
+            c_set['set_no'] = set_no
+            set_no += 1
+            sets_planned.append(c_set)
+        elif match[1] == 4:
+            multiset_reps = int(match[0].group(1).replace(',', '.'))
+            multiset_rpe_list = match[0].group(0).split('@')[1:]
+            for rpe in multiset_rpe_list:
+                c_set = {'reps': '', 'weight':'', 'RPE': '', 'set_no': ''}
+                c_set['reps'] = multiset_reps
+                c_set['RPE'] = float(rpe.replace(',','.'))
+                c_set['set_no'] = set_no
+                set_no += 1
+                sets_done.append(c_set)
+        elif match[1] == 5:
+            multiset_sets = int(match[0].group(1).replace(',', '.'))
+            multiset_reps = int(match[0].group(2).replace(',', '.'))
+            multiset_rpe = float(match[0].group(3).replace(',', '.'))
+            for _ in range(0,multiset_sets):
+                c_set = {'reps': '', 'weight':'', 'RPE': '', 'set_no': ''}
+                c_set['reps'] = multiset_reps
+                c_set['set_no'] = set_no
+                c_set['RPE'] = multiset_rpe
+                set_no += 1
+                sets_done.append(c_set)
+        elif match[1] == 6:
+            multiset_sets = int(match[0].group(1).replace(',', '.'))
+            multiset_rpe = float(match[0].group(2).replace(',', '.'))
+            for _ in range(0,multiset_sets):
+                c_set = {'reps': '', 'weight':'', 'RPE': '', 'set_no': ''}
+                c_set['set_no'] = set_no
+                c_set['RPE'] = multiset_rpe
+                set_no += 1
+                sets_done.append(c_set)
+        elif match[1] == 7:
+            c_set['weight'] = float(match[0].group(1).replace(',','.'))
+            c_set['reps'] = int(match[0].group(2).replace(',', '.'))
+            c_set['set_no'] = set_no
+            set_no += 1
+            sets_planned.append(c_set)
+        elif match[1] == 8:
+            pass
+        elif match[1] == 9:
+            multiset_sets = int(match[0].group(1).replace(',', '.'))
+            multiset_weight = float(match[0].group(2).replace(',','.'))
+            multiset_rpe = float(match[0].group(3).replace(',', '.'))
+            for _ in range(0,multiset_sets):
+                c_set = {'reps': '', 'weight':'', 'RPE': '', 'set_no': ''}
+                c_set['weight'] = multiset_weight
+                c_set['RPE'] = multiset_rpe
+                c_set['set_no'] = set_no
+                set_no += 1
+                sets_done.append(c_set)
+
 
 class Workout:
     def __init__(self, day, date, exercises, length, notes):
@@ -220,6 +314,7 @@ class Workout:
         self.length = length
         self.note = note
 
+
 class Microcycle:
     def __init__(self, date_start, date_end, workouts, drugs, notes):
         self.length = date_end - date_start
@@ -228,6 +323,7 @@ class Microcycle:
         self.workouts = workouts
         self.drugs = drugs
         self.notes = notes
+
 
 class Mesocycle:
     def __init__(self, microcycles, date_start, date_end, notes):
@@ -244,5 +340,6 @@ if __name__ == '__main__':
     wb = load_workbook(filename = XSL_FILE)
     ws = wb['program']
 
-    parse_mesocycle(ws, 'A', '1')
+    tsp = TrainingSpreadsheetParser()
+    tsp.parse_mesocycle(ws, 'A', '1')
 
