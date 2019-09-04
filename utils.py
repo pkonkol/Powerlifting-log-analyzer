5141b04273c91c8c7b1f56356c4f5bdfd46e2dbf
Sequindo <chacper@wp.pl>
2019-09-05 01:56:25 +0200

    Update utils.py
    Unit conversion with bias.

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
+import math
 
+def roundWithBias(x):
+    y = round(math.ceil(x)-x,1)
+    if y > 0.5:
+        return round(x,1)-(1-y)
+    else:
+        return round(x,1)+y
 
 def kg_to_lbs(kg):
-    pass
+    return roundWithBias(kg*2.204)
 
 def lbs_to_kg(kg):
-    pass
+    return roundWithBias(kg/2.204)
